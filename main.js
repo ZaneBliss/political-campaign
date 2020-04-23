77265b1939946a51d9d2d89224213b058860aadf
zanebliss <zanebliss@icloud.com>
2020-04-22 19:20:13 -0500

    init commit

---
+++ main.js
@@ -0,0 +1,39 @@
+
+const candidate = {
+    district: "5th congressional district",
+    platformStatements: {
+        taxes: "expand taxes",
+        jobs: "expand jobs",
+        infrastructure: "improve infrastructure",
+        healthCare: "reduce cost",
+        crime: "reduce law enforcement budget",
+    },
+    biography: "Born in a barn",
+    imageGallery: [
+        "",
+        "",
+        "",
+    ],
+    missionStatement: "To bodly go",
+}
+
+const candidateResources = {
+    donationForm: "giphy.com",
+    votingURL: "facebook.com",
+    calendarOfEvents: [
+        "January 13th speaking engagement",
+        "January 26th voting on a bill",
+        "February 2nd ball"
+    ]
+}
+
+const volunteers = [
+    volunteer1 = {
+        name: "John Doe",
+        address: "1234 Special address",
+        email: "email@email.com",
+        phone: "123456790",
+        availability: "full-time",
+        prefers: "answering phones"
+    }
+]
